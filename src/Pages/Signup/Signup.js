34004faec7ed5d8544a95bf9e4feb15e3d02f62b
akocerke <152269613+akocerke@users.noreply.github.com>
2024-02-20 11:24:33 +0100

    update Signup

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -4,16 +4,18 @@ import React, { useState } from 'react';
 import styles from './Signup.module.css'; // Importieren Sie das CSS-Modul
 
 const Signup = () => {
-    // Zustände für Benutzername, E-Mail und Passwort
-    const [username, setUsername] = useState('');
+    // Zustände für Vorname, Nachname, E-Mail und Passwort
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     // Funktion zum Einreichen des Formulars
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Hier können Sie den Benutzernamen, die E-Mail und das Passwort überprüfen und die entsprechenden Aktionen ausführen, z. B. eine Anfrage an den Server senden
-        console.log('Username:', username);
+        // Hier können Sie den Vorname, Nachname, die E-Mail und das Passwort überprüfen und die entsprechenden Aktionen ausführen, z. B. eine Anfrage an den Server senden
+        console.log('First Name:', firstName);
+        console.log('Last Name:', lastName);
         console.log('Email:', email);
         console.log('Password:', password);
         // Beispiel: Ein Anfrage an den Server senden, um sich zu registrieren
@@ -24,8 +26,12 @@ const Signup = () => {
             <form className={styles.form} onSubmit={handleSubmit}>
                 <h2>Sign Up</h2>
                 <div className={styles.inputGroup}>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <label htmlFor="firstName">Vorname:</label>
+                    <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                </div>
+                <div className={styles.inputGroup}>
+                    <label htmlFor="lastName">Name:</label>
+                    <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
                 </div>
                 <div className={styles.inputGroup}>
                     <label htmlFor="email">Email:</label>
