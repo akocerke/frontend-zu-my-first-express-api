de423692d6a27a1493ce79c33ffb33e03699eaba
akocerke <152269613+akocerke@users.noreply.github.com>
2024-02-21 17:18:26 +0100

    Create todo implementiert

--- src/Pages/Todo/CreateTodo.js
+++ src/Pages/Todo/CreateTodo.js
@@ -7,6 +7,9 @@ const CreateTodo = () => {
   // Zustände für die Eingabefelder
   const [title, setTitle] = useState('');
   const [completed, setCompleted] = useState(false);
+  const [doneByDate, setDoneByDate] = useState('');
+  const [userId, setUserId] = useState(''); // Hinzufügen von userId-Zustand für das Todo-Formular
+  const [todoAdded, setTodoAdded] = useState(false); // Zustand für die Anzeige der Bestätigungsnachricht
 
   // Funktion zum Bearbeiten des Formulars und Senden der POST-Anfrage
   const handleSubmit = async (e) => {
@@ -14,11 +17,13 @@ const CreateTodo = () => {
 
     try {
       // Sende die POST-Anfrage an den Server
-      const response = await axios.post('/api/todos/create', { title, completed });
+      const response = await axios.post('http://localhost:3030/v1/todos/create', { userId, title, completed, doneByDate });
       console.log(response.data); // Gib die Antwort in der Konsole aus
       // Setze die Eingabefelder zurück nach erfolgreicher Erstellung
       setTitle('');
       setCompleted(false);
+      setDoneByDate('');
+      setTodoAdded(true); // Setze todoAdded auf true, um die Bestätigungsnachricht anzuzeigen
     } catch (error) {
       console.error('Fehler beim Hinzufügen eines neuen Todos:', error);
     }
@@ -28,7 +33,12 @@ const CreateTodo = () => {
     <Content>
       <div className={styles.container}>
         <h2>Neues Todo erstellen</h2>
+        {todoAdded && <p>Todo wurde erfolgreich hinzugefügt!</p>} {/* Zeige die Bestätigungsnachricht, wenn ein Todo hinzugefügt wurde */}
         <form onSubmit={handleSubmit} className={styles.form}>
+          <label className={styles.label}>
+            Benutzer-ID:
+            <input type="text" value={userId} onChange={(e) => setUserId(e.target.value)} className={styles.input} />
+          </label>
           <label className={styles.label}>
             Titel:
             <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className={styles.input} />
@@ -36,6 +46,10 @@ const CreateTodo = () => {
           <label className={styles.label}>
             Abgeschlossen:
             <input type="checkbox" checked={completed} onChange={(e) => setCompleted(e.target.checked)} />
+          </label>
+          <label className={styles.label}>
+            Fälligkeitsdatum:
+            <input type="date" value={doneByDate} onChange={(e) => setDoneByDate(e.target.value)} className={styles.input} />
           </label>
           <button type="submit" className={styles.button}>Todo hinzufügen</button>
         </form>
