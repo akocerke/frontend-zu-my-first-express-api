2346e6cc3bb8cbdb34cb44d902502aebad3e2bcf
akocerke <152269613+akocerke@users.noreply.github.com>
2024-02-26 15:43:18 +0100

    Lodin implementiert

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,24 @@
 // Login.js
 
 import React, { useState } from 'react';
-import styles from './Login.module.css'; // Importieren Sie das CSS-Modul
+import styles from './Login.module.css';
+import axios from 'axios';
 
 const Login = () => {
-    // Zustände für Benutzername und Passwort
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    // Funktion zum Einreichen des Formulars
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Hier können Sie den Benutzernamen und das Passwort überprüfen und die entsprechenden Aktionen ausführen, z. B. eine Anfrage an den Server senden
-        console.log('Username:', username);
-        console.log('Password:', password);
-        // Beispiel: Ein Anfrage an den Server senden, um sich einzuloggen
+        try {
+            const response = await axios.post('http://localhost:3030/v1/auth/login', {
+                email: email,
+                password: password
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error('Fehler bei der Anmeldung:', error);
+        }
     };
 
     return (
@@ -22,11 +26,11 @@ const Login = () => {
             <form className={styles.form} onSubmit={handleSubmit}>
                 <h2>Login</h2>
                 <div className={styles.inputGroup}>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <label htmlFor="email">E-Mail:</label>
+                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className={styles.inputGroup}>
-                    <label htmlFor="password">Password:</label>
+                    <label htmlFor="password">Passwort:</label>
                     <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <button type="submit">Login</button>
